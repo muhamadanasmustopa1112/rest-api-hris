e67e7eada74904afbc21597e5cbbdd46df56391e
Muhamad Anas Mustopa <muhamadanasmustopa1112@gmail.com>
2025-07-16 14:20:19 +0700

    feat : update class validator bussines trip

--- app/Http/Controllers/PerjalananDinasController.php
+++ app/Http/Controllers/PerjalananDinasController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\PerjalananDinas;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Validator;
 
 class PerjalananDinasController extends Controller
 {
